7b951f2ca83c51eb960f0ab6633be5ba6f44ecdb
Katerina <ekurochka@gmail.com>
2018-06-18 14:26:20 +0300

    Задача 1. Конвертер

---
+++ hex2rgb/js/HexInput.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const HexInput = props => {
+  function onChange(event) {
+    props.onChange(event.currentTarget.value);
+  }
+
+  return (
+    <input
+      value={props.value}
+      onChange={onChange}
+      type="text"
+      className="hex-field js-hex-field"
+      placeholder="#000000" />
+  );
+};
